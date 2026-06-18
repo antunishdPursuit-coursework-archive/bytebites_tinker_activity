892f60063c9a050b7fd7404998b4625b369a6f3b
antunishdPursuit <122734610+antunishdPursuit@users.noreply.github.com>
2026-06-18 03:12:41 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,20 @@
+# ByteBites core classes:
+# Customer tracks a customer's name and purchase history.
+# FoodItem represents one menu item with name, price, category, and popularity rating.
+# Menu stores food items and helps filter/sort them.
+# Transaction stores selected items for a customer and calculates the total cost.
+
+class Customer:
+    pass
+
+
+class FoodItem:
+    pass
+
+
+class Menu:
+    pass
+
+
+class Transaction:
+    pass
